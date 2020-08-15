88532c5e88e5370dc80561c69dadab2322dcf9b4
Christopher Dutcher <cadutcher@icloud.com>
2020-08-15 14:04:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
-doc = Nokogiri::HTML(open("https://www.gamesradar.com/upcoming-xbox-one-games/")
+doc = Nokogiri::HTML(open("https://www.gamesradar.com/upcoming-xbox-one-games/"))
 # doc.css(".headline-260IBN").text
 binding.pry
